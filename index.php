fb98523697b2796395784b88cc64c8ef7369e223
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-16 18:35:54 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$welcomeMessage = 'Hello Wilder !!!'
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Retour vers le futur</title>
+</head>
+<body>
+
+    <h1><?= $welcomeMessage ?></h1>
+    
+</body>
+</html>
