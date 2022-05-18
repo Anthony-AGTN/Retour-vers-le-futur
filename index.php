ae6b7fa6fd03424a4a6c4b28cb1af929740ac7f9
Anthony GOUTON <anthony.gouton.wcs@gmail.com>
2022-05-18 18:27:24 +0200

    add index.php style.css and digital font-family

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
 
-$welcomeMessage = 'Hello Wilder !!!'
+    $presentTime = new DateTime();
+    $destinationTime = new DateTime('1988-07-20');
+
+    $presentTime = $presentTime->format('F d Y A g:i')
+
 
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Retour vers le futur</title>
 </head>
+
 <body>
 
-    <h1><?= $welcomeMessage ?></h1>
-    
+    <div class="dashBoard">
+        <p><?= $presentTime ?></p>
+    </div>
+
 </body>
+
 </html>
